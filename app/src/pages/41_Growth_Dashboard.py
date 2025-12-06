461a18219f44977006f9f1072dbb8c39ceae6665
Alex Zheng <alexanderyzheng@gmail.com>
2025-12-06 15:52:34 -0500

    setup PM pages

---
+++ app/src/pages/41_Growth_Dashboard.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import requests
+from datetime import datetime, timedelta
+from modules.nav import SideBarLinks
+
+SideBarLinks(show_home=True)
+
+st.title("📈 Growth Dashboard")
+
+# API URL
+API_URL = "http://web-api:4000"
